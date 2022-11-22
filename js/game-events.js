6e13e2e53cc979940c8484824a7264c8b47c506b
Erick B. Gómez <erickb.gomez28@gmail.com>
2022-11-22 13:01:13 -0600

    Add data-state attribute to manipulate buttons
    Now it is easier to manipulate the 3 states of a button, I'm using
    a data attribute called "data-state". I can change the button's
    state from one to another without the need to remove and add classes.

--- js/game-events.js
+++ js/game-events.js
@@ -1,23 +1,22 @@
-const playerButtons = document.querySelectorAll("button.player-button");
-const computerButtons = document.querySelectorAll("button.computer-button");
+// Select buttons
+const playerButtons = document.querySelectorAll(".player-button");
+const computerButtons = document.querySelectorAll(".computer-button");
+const allButtons = document.querySelectorAll(".action-block");
 
 resetAllButtons();
 
 function selectPlayerButton(e) {
+    changeButtonState(e.target, "selected");
+
+    // Save player selection
     playerSelection = e.target.textContent;
 
-    // Clicked button will change its state to "Selected"
-    e.target.classList.add("selected");
-
-    console.log("Player selection: " + playerSelection);
+    console.log("Player selection: " + playerSelection);// Test
 
     // Disable all buttons except the one who invoked this function
     playerButtons.forEach(playerButton => {
-
         if (playerButton !== e.target) {
-
-            playerButton.classList.remove("player-button");
-            playerButton.classList.add("player-button-inactive");
+            changeButtonState(playerButton, "inactive");
             playerButton.removeEventListener("click", selectPlayerButton);
         }
     });
@@ -31,42 +30,27 @@ function selectComputerButton(playerSelection) {
     
     computerButtons.forEach(computerButton => {
         if (playerSelection === computerButton.textContent) {
-            computerButton.classList.add("selected");
+            changeButtonState(computerButton, "selected")
 
             computerSelection = computerButton.textContent;
             console.log("Computer selection: " + computerSelection);
 
         } else {
-            computerButton.classList.remove("computer-button");
-            computerButton.classList.add("computer-button-inactive");
+            changeButtonState(computerButton, "inactive");
         }
     })
 }
 
+function changeButtonState(button, newState) {
+    button.dataset.state = newState;
+}
+
 function resetAllButtons() {
-    playerButtons.forEach(playerButton => {
-        playerButton.addEventListener("click", selectPlayerButton, {
+    allButtons.forEach(button => changeButtonState(button, "active"));
+
+    playerButtons.forEach(playerButton => playerButton.addEventListener("click", selectPlayerButton, {
             once: true
-        });
-
-        // Reset selected button
-        if (playerButton.classList.contains("selected")) playerButton.classList.remove("selected");
-
-        // Enable all buttons by resetting their classes
-        if (playerButton.classList.contains("player-button-inactive")) {
-            playerButton.classList.remove("player-button-inactive");
-            playerButton.classList.add("player-button");
-        }
-    });
-
-    computerButtons.forEach(computerButton => {
-        if (computerButton.classList.contains("selected")) computerButton.classList.remove("selected");
-    
-        if (computerButton.classList.contains("computer-button-inactive")) {
-            computerButton.classList.remove("computer-button-inactive");
-            computerButton.classList.add("computer-button");
-        }
-    })
+    }));
 }
 
 // Reset test
